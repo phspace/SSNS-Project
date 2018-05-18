187b7968bd0c800b284d7a5af1c2bd1dcb3eddf5
Pham Tien Hung <t.pham@reply.de>
2018-05-18 18:54:00 +0200

    Small changes for accelerometer

--- src/main/java/com/hungpham/UI/RTGraph.java
+++ src/main/java/com/hungpham/UI/RTGraph.java
@@ -28,12 +28,12 @@ public class RTGraph implements Runnable {
     private void go() {
 
         // Create Chart
-        chart = QuickChart.getChart("SwingWorker XChart Real-time Demo", "Time", "Value", "randomWalk", new double[] { 0 }, new double[] { 0 });
+        chart = QuickChart.getChart("Accelerometer Z-axis", "Time", "Value", "acc_z", new double[] { 0 }, new double[] { 0 });
         chart.getStyler().setLegendVisible(false);
         chart.getStyler().setXAxisTicksVisible(false);
 
         // Show it
-        sw = new SwingWrapper<XYChart>(chart);
+        sw = new SwingWrapper<>(chart);
         sw.displayChart();
 
         graphWorker = new GraphWorker();
@@ -59,14 +59,14 @@ public class RTGraph implements Runnable {
             while (!isCancelled() ) {
                 int count = 0;
                 double[] array = new double[300];
-                while (count < 10) {
+                //while (count < 10) {
                     String accz = utils.TCPReceive(Definitions.GRAPH_PORT);
                     fifo.add(Double.parseDouble(accz));
                     if (fifo.size() > 300) {
                         fifo.removeFirst();
                     }
-                    count++;
-                }
+                    //count++;
+                //}
                 for (int i = 0; i < fifo.size(); i++) {
                     array[i] = fifo.get(i);
                 }
@@ -79,21 +79,10 @@ public class RTGraph implements Runnable {
         @Override
         protected void process(List<double[]> chunks) {
 
-            //System.out.println("number of chunks: " + chunks.size());
-
             double[] mostRecentDataSet = chunks.get(chunks.size() - 1);
 
-            chart.updateXYSeries("randomWalk", null, mostRecentDataSet, null);
+            chart.updateXYSeries("acc_z", null, mostRecentDataSet, null);
             sw.repaintChart();
-
-            long start = System.currentTimeMillis();
-            long duration = System.currentTimeMillis() - start;
-            try {
-                Thread.sleep(20 - duration); // 40 ms ==> 25fps
-                // Thread.sleep(400 - duration); // 40 ms ==> 2.5fps
-            } catch (InterruptedException e) {
-            }
-
         }
     }
 }
