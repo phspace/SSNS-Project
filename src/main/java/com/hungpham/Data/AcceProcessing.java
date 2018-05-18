14ac31e4fb1b52582d40ebd96a823fcc0807baf8
Pham Tien Hung <t.pham@reply.de>
2018-05-18 10:50:00 +0200

    Add Kalman Filter

--- src/main/java/com/hungpham/Data/AcceProcessing.java
+++ src/main/java/com/hungpham/Data/AcceProcessing.java
@@ -1,5 +1,6 @@
 package com.hungpham.Data;
 
+import com.hungpham.Algorithms.KalmanFilter;
 import com.hungpham.Controller.SerialPortController;
 import com.hungpham.Utils.Utils;
 import gnu.io.SerialPort;
@@ -9,32 +10,44 @@ import java.util.ArrayList;
 public class AcceProcessing implements Runnable {
 
     private double[] acce;
+    private double[] filtered;
     private double[] offsets;
     private int samples;
+
+    private KalmanFilter[] kalmanFilters;
 
     public AcceProcessing() {
         acce = new double[3];
         offsets = new double[3];
+        filtered = new double[3];
         samples = 0;
+        kalmanFilters = new KalmanFilter[3];
     }
 
     private void convertData() {
 
-            ArrayList<String> hexList = null;
-            try {
-                hexList = Utils.seperate4Hex(AcceObserver.acceData.take());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            for (String s : hexList) {
-                int i = 0;
-                String reverse;
-                reverse = s.substring(2, 4) + s.substring(0, 2);
-                acce[i] = (Utils.hexStringToInt(reverse) * 1.0) / (32768 / 8);
-                System.out.println("Accelerometer value: " + acce[i]);
-                i++;
-            }
+        ArrayList<String> hexList = null;
+        try {
+            hexList = Utils.seperate4Hex(AcceObserver.acceData.take());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (String s : hexList) {
+            int i = 0;
+            String reverse;
+            reverse = s.substring(2, 4) + s.substring(0, 2);
+            acce[i] = (Utils.hexStringToInt(reverse) * 1.0) / (32768 / 8);
+            //System.out.println("Accelerometer value: " + acce[i]);
+            i++;
+        }
 
+    }
+
+    public void kalmanFilter() {
+        for (int i = 0; i < 3; i++) {
+            filtered[i] = kalmanFilters[i].kalman_filter_update(acce[i]);
+            System.out.println("Filtered Accelerometer value: " + acce[i]);
+        }
     }
 
     private void calibrateAcce() {
@@ -46,9 +59,9 @@ public class AcceProcessing implements Runnable {
             sums[2] += acce[2];
             samples++;
         }
-        offsets[0] = sums[0]/100 - 0;
-        offsets[1] = sums[1]/100 - 0;
-        offsets[2] = sums[2]/100 - 1;
+        offsets[0] = sums[0] / 100 - 0;
+        offsets[1] = sums[1] / 100 - 0;
+        offsets[2] = sums[2] / 100 - 1;
     }
 
     public void printData() {
@@ -60,8 +73,12 @@ public class AcceProcessing implements Runnable {
 
     @Override
     public void run() {
+        kalmanFilters[0] = new KalmanFilter(0.1, 100, 100, 0);
+        kalmanFilters[1] = new KalmanFilter(0.1, 100, 100, 0);
+        kalmanFilters[2] = new KalmanFilter(0.1, 100, 100, 1);
         while (true) {
             convertData();
+            kalmanFilter();
         }
     }
 }
