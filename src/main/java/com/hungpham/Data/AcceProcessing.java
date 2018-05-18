eea6f960f6d8f3171489339a11198d78183f8e63
Pham Tien Hung <t.pham@reply.de>
2018-05-18 12:11:57 +0200

    Remove Kalman Filter and add Calibration

--- src/main/java/com/hungpham/Data/AcceProcessing.java
+++ src/main/java/com/hungpham/Data/AcceProcessing.java
@@ -22,9 +22,12 @@ public class AcceProcessing implements Runnable {
         filtered = new double[3];
         samples = 0;
         kalmanFilters = new KalmanFilter[3];
+        offsets[0] = 0;
+        offsets[1] = 0;
+        offsets[2] = 0;
     }
 
-    private void convertData() {
+    private void readRaw() {
 
         ArrayList<String> hexList = null;
         try {
@@ -32,53 +35,62 @@ public class AcceProcessing implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        int i = 0;
         for (String s : hexList) {
-            int i = 0;
             String reverse;
             reverse = s.substring(2, 4) + s.substring(0, 2);
-            acce[i] = (Utils.hexStringToInt(reverse) * 1.0) / (32768 / 8);
-            //System.out.println("Accelerometer value: " + acce[i]);
+            acce[i] = (Utils.hexStringToInt(reverse) * 1.0) - offsets[i];
+            //acce[i] = (Utils.hexStringToInt(reverse) * 1.0) / (32768 / 8);
+            if (acce[i] > 32768) {
+                acce[i] = acce[i] - 65536;
+            }
+            //System.out.println("Accelerometer value: " + i + "   " + acce[i]);
             i++;
         }
-
     }
 
     public void kalmanFilter() {
         for (int i = 0; i < 3; i++) {
             filtered[i] = kalmanFilters[i].kalman_filter_update(acce[i]);
-            System.out.println("Filtered Accelerometer value: " + acce[i]);
+            filtered[i] = filtered[i] / (32768 / 8);
+            System.out.println("Filtered Accelerometer value: " + i + "   " + filtered[i]);
         }
     }
 
     private void calibrateAcce() {
         double[] sums = new double[3];
-        while (samples < 100) {
-            convertData();
+        while (samples < 50) {
+            readRaw();
             sums[0] += acce[0];
             sums[1] += acce[1];
             sums[2] += acce[2];
             samples++;
         }
-        offsets[0] = sums[0] / 100 - 0;
-        offsets[1] = sums[1] / 100 - 0;
-        offsets[2] = sums[2] / 100 - 1;
+        offsets[0] = sums[0] / 50 - 0;
+        offsets[1] = sums[1] / 50 - 0;
+        offsets[2] = sums[2] / 50 - 4022;
     }
 
     public void printData() {
         for (int i = 0; i < 3; i++) {
-            acce[i] = acce[i] - offsets[i];
-            System.out.println("Accelerometer value: " + acce[i]);
+            acce[i] = acce[i] / (32768 / 8);
+            System.out.println("Accelerometer value: " + i + "   " + acce[i]);
         }
     }
 
     @Override
     public void run() {
-        kalmanFilters[0] = new KalmanFilter(0.1, 100, 100, 0);
-        kalmanFilters[1] = new KalmanFilter(0.1, 100, 100, 0);
-        kalmanFilters[2] = new KalmanFilter(0.1, 100, 100, 1);
         while (true) {
-            convertData();
-            kalmanFilter();
+            if (AcceObserver.acceData.size() > 0) {
+                if (samples < 50) {
+                    System.out.println("calibrating...");
+                    calibrateAcce();
+                }
+                readRaw();
+                printData();
+                //kalmanFilter();
+            }
+
         }
     }
 }
