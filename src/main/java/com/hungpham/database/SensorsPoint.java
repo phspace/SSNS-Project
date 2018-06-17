c80fbd3c6343f0e7fd2e05f9056d21d0bd87359b
Pham Tien Hung <t.pham@reply.de>
2018-06-17 14:03:19 +0200

    Add points for SensorPoint

--- src/main/java/com/hungpham/database/SensorsPoint.java
+++ src/main/java/com/hungpham/database/SensorsPoint.java
@@ -20,6 +20,27 @@ public class SensorsPoint {
     @Column(name = "conn")
     private String conn;
 
+    @Column(name = "acc_x")
+    private double acc_x;
+
+    @Column(name = "acc_y")
+    private double acc_y;
+
+    @Column(name = "acc_z")
+    private double acc_z;
+
+    public double getAcc_x() {
+        return acc_x;
+    }
+
+    public double getAcc_y() {
+        return acc_y;
+    }
+
+    public double getAcc_z() {
+        return acc_z;
+    }
+
     public double getBaro() {
         return baro_value;
     }
